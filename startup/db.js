2cee02f9c5c45c11bc54585d536c184baa5a017c
PlayerZaf <playerzaf.contact@gmail.com>
2019-07-11 17:38:26 +0530

    mongodb linked

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 const startupDebugger = require('debug')('app:startup');
 module.exports = function () {
-
-mongoose.connect('mongodb://localhost/Aquses')
+        mongoose.connect('mongodb+srv://abhishek8938:gotranks@cluster0-scnj3.mongodb.net/test?retryWrites=true')
         .then(() => console.log('connected to mongodb'))
-        .catch(err=> startupDebugger('could not connect',err))
+        .catch(err=> console.log('could not connect',err))
+        
+// mongoose.connect('mongodb://localhost/Aquses')
+//         .then(() => console.log('connected to mongodb'))
+//         .catch(err=> console.log('could not connect',err))
 }
