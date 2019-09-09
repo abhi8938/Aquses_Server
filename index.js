df454041c01e53d56341c3027acf0a1f1352b7a9
PlayerZaf <playerzaf.contact@gmail.com>
2019-09-09 13:47:03 +0530

    4th

--- index.js
+++ index.js
@@ -4,21 +4,20 @@ const bodyParser = require('body-parser');
 const engines = require('consolidate');
 const cors = require('cors');
 const app = express();
-
+redirect(app); 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.engine("ejs", engines.ejs);
+app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+// app.use(express.static('public'));
+
 app.use(cors());
-app.use(express.static('public'));
-
-
-app.use(express.static(__dirname + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({extended:true})); 
+// app.use(express.json());
+// app.use(express.urlencoded({extended:true})); 
 
 // if(app.get('env') === 'development'){
 // app.use(morgan('tiny'));
